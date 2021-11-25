0befb95be48b082d7bfa6f00b1dd201184e9e9e2
Mizanurrahman119 <niloy719119@gmail.com>
2021-11-26 01:55:58 +0600

    addit middle ware

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-//middleware 
+//--------set middleware--------- 
 app.use(cors());
 app.use(express.json());
 
